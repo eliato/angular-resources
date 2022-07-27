aadc072c05858e716e16b35b3df25868586eca46
eliato <eliato_@hotmail.com>
2022-07-26 22:42:57 -0600

    add lazy

--- lazy-modules/src/app/app-routing.module.ts
+++ lazy-modules/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosComponent } from './core/mantenimientos/usuarios/usuarios.component';
 
 const routes: Routes = [
   {
     path: 'mantenimientos',
-    loadChildren: ()=>  import('./core/mantenimientos/usuarios/usuarios.module').then
-    (m => m.UsuariosModule)
-  }
+   //component: UsuariosComponent
+      loadChildren: ()=>  import('./core/mantenimientos/usuarios/usuarios.module').then
+     (m => m.UsuariosModule)
+   }
 ];
 
 @NgModule({
