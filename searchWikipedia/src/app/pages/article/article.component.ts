fa1a7d3d67be241d11fe63973f85e135c7d59d68
eliato <eliato_@hotmail.com>
2022-10-10 18:33:50 -0600

    update c articles

--- searchWikipedia/src/app/pages/article/article.component.ts
+++ searchWikipedia/src/app/pages/article/article.component.ts
@@ -3,10 +3,10 @@ import { Article } from "../services/search.service";
 @Component({
   selector: 'app-article',
   template: `
-             <li class="w-full text-gray-700 p-4 mt-2 bg-white cursor-pointer hover:bg-gray-100">
-              <a [href]="'https://es.wikipedia.org/?curid='+ article.pageid" target="_blank" class="text-blue-600" >
-                {{article.title}}
-              </a>
+             <li class="w-full text-gray-700 p-4 mt-2 bg-white cursor-pointer hover:bg-gray-100" (click)="open()">
+             <h4 class="text-blue-500">
+             {{article.title}}
+             </h4>          
               <p [innerHTML]="article.snippet"></p>
              </li>
                 
@@ -17,5 +17,8 @@ export class ArticleComponent  {
   @Input() article!: Article;
   constructor() { }
 
+  open(){
+    window.open(`https://es.wikipedia.org/?curid=${this.article.pageid}`)
+  }
 
 }
