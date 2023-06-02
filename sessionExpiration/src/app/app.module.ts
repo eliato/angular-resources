f3f98ecaff2fbd403b87b7f80e1d6ae7c52222a9
Elias Torres <alexander.torres@bcr.gob.sv>
2023-06-02 10:15:47 -0600

    update

--- sessionExpiration/src/app/app.module.ts
+++ sessionExpiration/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    SessionExpirationAlert.forRoot({totalMinutes: 0.5}),
+    SessionExpirationAlert.forRoot({totalMinutes: 1}),
     
   ],
   providers: [
