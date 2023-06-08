8ada22135e33db9c961333fdd8c5fccf570b6df0
Elias Torres <eliato_@hotmail.com>
2023-06-08 10:24:06 -0600

    adding auth module

--- adminProTemplate/src/app/pages/pages.module.ts
+++ adminProTemplate/src/app/pages/pages.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
-import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
@@ -19,7 +21,7 @@ import { AppRoutingModule } from '../app-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    AppRoutingModule
+    RouterModule
   ],
   exports: [
     DashboardComponent,
