6c114f2ac8960e02a3f85cc821dfa28602de553c
Elias Torres <eliato_@hotmail.com>
2023-06-15 15:03:47 -0600

    implement grafica

--- adminProTemplate/src/app/pages/grafica1/grafica1.component.ts
+++ adminProTemplate/src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-grafica1',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./grafica1.component.css']
 })
 export class Grafica1Component {
+
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ],
+        backgroundColor: ['#6857E6', '#009FEE', '#f02059']  },
+
+    ]
+  };
+
+  //public doughnutChartType: ChartType = 'doughnut';
+
 
 }
