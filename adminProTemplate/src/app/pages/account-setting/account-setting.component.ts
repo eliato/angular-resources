6acb75da27845621fb4307c82b5106ae034a3917
Elias Torres <eliato_@hotmail.com>
2023-06-19 10:27:17 -0600

    adding check theme selected

--- adminProTemplate/src/app/pages/account-setting/account-setting.component.ts
+++ adminProTemplate/src/app/pages/account-setting/account-setting.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account-setting',
   templateUrl: './account-setting.component.html',
   styleUrls: ['./account-setting.component.css']
 })
-export class AccountSettingComponent {
+export class AccountSettingComponent implements OnInit {
 
   public urlTheme = document.querySelector('#theme');
+
+  public links!: NodeListOf<Element>;
+
+  ngOnInit() {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
+
 
   changeTheme( theme: string ){
 
@@ -18,8 +26,30 @@ export class AccountSettingComponent {
 
     localStorage.setItem('theme', url);
 
+    this.checkCurrentTheme();
+
   }
 
+  checkCurrentTheme(){
+
+    this.links.forEach( elem => {
+
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeURl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.urlTheme?.getAttribute('href');
+
+      if (btnThemeURl === currentTheme) {
+        elem.classList.add('working');
+      }
+
+
+
+    }
+
+    )
+
+  }
 
 
 }
