9151a6f0389518593bf3925ac002f89d586cb751
Elias Torres <eliato_@hotmail.com>
2023-07-03 11:55:30 -0600

    optimization breadcrumbs

--- adminProTemplate/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ adminProTemplate/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,32 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {  ActivationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { Subscription, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
   title!:string;
+  titleSubscribe$!: Subscription;
+
   constructor(private router: Router){
-   this.getTitleRoute();
+  this.titleSubscribe$ = this.getTitleRoute().subscribe(
+    ({titulo}) =>{
+     this.title = titulo;
+     document.title = `AdminPro - ${this.title}`
+    }
+   )
+  }
+
+
+  ngOnDestroy(): void {
+    this.titleSubscribe$.unsubscribe();
   }
 
   getTitleRoute(){
-    this.router.events
+  return this.router.events
     .pipe(
       filter<any>(event => event instanceof ActivationEnd),
       filter((event:ActivationEnd) => event.snapshot.firstChild === null),
       map((event:ActivationEnd) => event.snapshot.data)
 
-    )
-    .subscribe(
-     ({titulo}) =>{
-      this.title = titulo;
-      document.title = `AdminPro - ${this.title}`
-     }
     )
   }
 
