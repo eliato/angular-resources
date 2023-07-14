9d69defa7d710f3f45fc281ebac9c3a5f5aace8a
Elias Torres <eliato_@hotmail.com>
2023-07-14 08:43:25 -0600

    validate email user

--- backendAdminPro/controllers/usuarios.js
+++ backendAdminPro/controllers/usuarios.js
@@ -1,5 +1,5 @@
 const Usuario = require('../models/usuarios');
-
+const { response } = require('express')
 
 const getUsuarios = async(req, res) => {
 
@@ -15,22 +15,36 @@ const getUsuarios = async(req, res) => {
 
 }
 
-const crearUsuarios = async(req, res) => {
+const crearUsuarios = async(req, res = response) => {
 
   const {nombre, email, password} = req.body;
 
-  const usuario = new Usuario(req.body);
+  try {
 
- await usuario.save();
-
-
-  res.json(
-    {
-      "ok":true,
-      usuario
-      
+    const existeEmail = await Usuario.findOne({ email });
+    if(existeEmail){ 
+      return res.status(400).json({
+        ok: false, 
+        msg: 'El correo ya exite'});
     }
-  )
+    const usuario = new Usuario(req.body);
+    await usuario.save();
+      res.json(
+        {
+          "ok":true,
+          usuario
+          
+        }
+      )
+    
+  } catch (error) {
+      res.status(500).json({
+        ok: false,
+        mg: 'Error inesperado verificar la consola'
+      })
+  }
+
+
 
 }
 
